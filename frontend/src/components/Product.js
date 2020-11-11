254e06fcf5e3f9e1ea7026c0c0f92400d0f38e8b
Tarun Soni <soni.tarun26@gmail.com>
2020-11-11 21:13:08 +0530

    added rating comp

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -15,9 +16,10 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} ⭐ from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reiews`}
+          />
         </Card.Text>
 
         <Card.Text as="h4">${product.price}</Card.Text>
