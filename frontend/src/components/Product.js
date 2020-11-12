2ea62d10ca54b5d0646feb8cd1ac592109c3549b
Tarun Soni <soni.tarun26@gmail.com>
2020-11-12 17:45:03 +0530

    added single product screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import Rating from "./Rating";
 import { Link } from "react-router-dom";
+import { Rating } from "./";
 
 const Product = ({ product }) => {
   return (
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Rating } from "../components";
+import products from "../products";
 
-const ProductScreen = () => {
-  return <div>ProductScreen</div>;
+import {
+  Button,
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap";
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return (
+    <>
+      <Link to="/" className="btn btn-light my-3 ">
+        <i className="fas fa-arrow-left mr-2" aria-hidden="true"></i>
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reiews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price : {product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              {/* price */}
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong style={{ letterSpacing: "0.4rem" }}>
+                      ${product.price}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
