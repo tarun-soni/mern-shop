2b599983212e324bdc909b80fbd3f0d3d88db3f6
Tarun Soni <soni.tarun26@gmail.com>
2020-11-17 22:18:16 +0530

    redux init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import store from "./store.js";
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import { Product } from "../components";
-import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productAction";
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
+    dispatch(listProducts());
+  }, [dispatch]);
 
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
   return (
     <>
       <h1>Latest Products</h1>
-
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>laoding..........</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Row>
+          {products?.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
