5089bdd89be0bd7bae5059f58633ae56da570266
Tarun Soni <soni.tarun26@gmail.com>
2020-11-22 22:05:16 +0530

    logout func

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <Navbar
       className="font-weight-bold"
@@ -23,12 +34,24 @@ const Header = () => {
                 CART
               </Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>
-                <i className="fas fa-user px-1"></i>
-                SIGN IN
-              </Nav.Link>
-            </LinkContainer>
+
+            {userInfo ? (
+              <NavDropdown title={userInfo.name?.toUpperCase()} id="username">
+                <LinkContainer to="/">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user px-1"></i>
+                  SIGN IN
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
