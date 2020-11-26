c4c3304fe9567b76818343a9107c8247960a2609
Tarun Soni <soni.tarun26@gmail.com>
2020-11-26 16:28:17 +0530

    getOrderById route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc    get order by id
+// @route   GET /api/orders/:id
+// @access  Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  //here populate works like key value pair
+  // 'from where', 'all values that needs to be populated'
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
