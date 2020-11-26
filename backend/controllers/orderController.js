e9ed379e722445f2b2186905ca5845c679c8bf85
Tarun Soni <soni.tarun26@gmail.com>
2020-11-27 00:34:44 +0530

    update Order to Paid route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import { set } from "mongoose";
 import Order from "../models/orderModel.js";
 
 // @desc    Create new order
@@ -55,5 +56,31 @@ const getOrderById = asyncHandler(async (req, res) => {
     throw new Error("Order not found");
   }
 });
+// @desc    update order to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private
 
-export { addOrderItems, getOrderById };
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+
+    order.paidAt = Date.now();
+
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updateOrder = await order.save();
+    res.json(404);
+  } else {
+    res.status(404);
+    throw new Error("Error in update order to paid");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
