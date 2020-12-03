04d4ff4656d7dbcb96b5e2d546d437a8fe39c932
Tarun Soni <soni.tarun26@gmail.com>
2020-12-03 18:22:07 +0530

    get all orders of a user

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 const router = express.Router();
 import {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+//tip :here keep all routes with /:id below
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
-
 export default router;
